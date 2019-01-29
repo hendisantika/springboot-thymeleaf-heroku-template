ef9ce4638d54924bf5379ba5e7c54897ac8b09bb
Hendi Santika <hendisantika@gmail.com>
2019-01-30 06:19:54 +0700

    #12 - Add Home Controller

---
+++ src/main/java/com/hendisantika/springbootthymeleafherokutemplate/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.hendisantika.springbootthymeleafherokutemplate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-heroku-template
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-30
+ * Time: 06:19
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    /**
+     * Show home page.
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public String index() {
+        logger.info("Showing home page");
+
+        return "home/index";
+    }
+}
