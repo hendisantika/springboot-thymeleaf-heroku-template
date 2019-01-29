6f22dec6b5877dae4e7bf8df8c004dbce394eebc
Hendi Santika <hendisantika@gmail.com>
2019-01-29 20:13:40 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootthymeleafherokutemplate/SpringbootThymeleafHerokuTemplateApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootthymeleafherokutemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafHerokuTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafHerokuTemplateApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hendisantika/springbootthymeleafherokutemplate/SpringbootThymeleafHerokuTemplateApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootthymeleafherokutemplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootThymeleafHerokuTemplateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
