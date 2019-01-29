06be23ef25367e2188f4da27e6e15865668b8f3b
Hendi Santika <hendisantika@gmail.com>
2019-01-30 06:20:22 +0700

    #13 - Add Sign Controller

---
+++ src/main/java/com/hendisantika/springbootthymeleafherokutemplate/controller/SigninController.java
@@ -0,0 +1,31 @@
+package com.hendisantika.springbootthymeleafherokutemplate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-heroku-template
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-30
+ * Time: 06:20
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class SigninController {
+    final Logger logger = LoggerFactory.getLogger(SigninController.class);
+
+    /**
+     * Sign in page.
+     */
+    @RequestMapping("/signin")
+    public String signin() {
+        logger.info("Showing sign in page");
+
+        return "signin/signin";
+    }
+}
