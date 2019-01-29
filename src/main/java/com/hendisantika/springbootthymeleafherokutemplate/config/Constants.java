a80a0a28e5978f4af9a37f5864033c47531384b1
Hendi Santika <hendisantika@gmail.com>
2019-01-29 20:19:11 +0700

    #4 - Add Constants Class

---
+++ src/main/java/com/hendisantika/springbootthymeleafherokutemplate/config/Constants.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootthymeleafherokutemplate.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-heroku-template
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-29
+ * Time: 20:18
+ * To change this template use File | Settings | File Templates.
+ */
+public final class Constants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+
+    private Constants() {
+    }
+
+}
